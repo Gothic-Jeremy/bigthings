3d2a23e92de688d870395a8ad78d586f8d833b13
Jeremy <gothicjeremy@163.com>
2020-11-21 17:59:27 +0800

    大事件第2天

--- index/index.js
+++ index/index.js
@@ -1,72 +1,70 @@
-// 设计第一层意思：
+// 第一层验证：是否有！
 if (!localStorage.getItem("token")) {
     location.href = "/login.html";
 }
-// ------------------------------请求个人信息
-$.ajax({
-        url: "http://ajax.frontend.itheima.net/my/userinfo",
-        // 设置请求头
-        headers: {
-            "Authorization": localStorage.getItem("token"),
-        },
+
+
+
+// ----------------------------------------------------请求个人信息
+window.get();
+
+function get() {
+    $.ajax({
+        url: "/my/userinfo",
         // 请求成功后调用
         success: function(res) {
             // console.log(res);
             if (res.status == 0) {
-                // 名称：有昵称就是昵称 没有就是用户名
+                // 名称：有昵称就昵称、不然就是用户名；
                 var name = res.data.nickname || res.data.username;
                 $(".username").text(name);
 
-                // 测试代码
+                // 测试代码：
                 // res.data.user_pic = undefined;
                 // name = "aaa";
 
                 // 头像：如果有头像数据
                 if (res.data.user_pic) {
+                    // 
                     $(".layui-nav-img").show().attr("src", res.data.user_pic);
                     $(".avatar").hide();
                 }
-                // 没有头像数据的时候
+                // 测试：没有头像数据的时候
                 else {
-                    // 截取name上的第一个字符
+                    // 截取name名字上第一个字符；
                     var t = name.substr(0, 1);
-                    // 英文字符：小写变大写：字符串 toUpperCase()
+                    // 英文字符：小写变为大写：字符串.toUpperCase()
                     t = t.toUpperCase();
-                    // show：会让元素变为行内元素
+
+                    // show:会让元素变为行内元素；
                     $(".avatar").show().css("display", "inline-block").text(t);
                     $(".layui-nav-img").hide()
                 }
+
             }
         },
-        // 请求失败后调用
-        fail: function() {},
-        // 完成：不管成功还是失败 都会执行这个函数
-        complete: function(xhr) {
-            // xhr：经过jQuery封装后 xhr对象
-            // 原生xhr 找出返回数据： xhr.reponseText
-            // xhr.responseJSON.
-            // console.log(xhr.responseJSON, 1111111111);
-            if (xhr.responseJSON.status == 1 || xhr.responseJSON.message == "身份认证失败！") {
-                localStorage.removeItem("token");
-                location.href = "/login.html";
-            }
-        }
     })
-    // ----------------------------------退出
-    // 1.点击退出
-    // 2.优化：弹窗 问是否退出
-    // 3.是
-    //     页面回到login
-    //     token：本地清空
+}
+
+
+
+// ----------------------------------------------------------退出
+//   1.点击退出
+//   2.优化：弹窗  问问是否退出？
+//   3.是：
+//      页面回到 login
+//      token：本地清空;
 $("#logout").on("click", function() {
-    layer.confirm('确定退出吗？', {
+    layer.confirm('您确认忍心退出么？', {
         icon: 3,
-        titlt: '退出窗口'
+        title: '退出窗口'
     }, function(index) {
+
         // 清空本地token
         localStorage.removeItem("token");
         location.href = "/login.html";
-        // index：number值 用户关闭窗口
+
+        // index: number值  用户关闭窗口！
         layer.close(index);
     });
 });
--- login/login.js
+++ login/login.js
@@ -1,74 +1,105 @@
-// ------------------------切换
-$('#login a').click(function() {
-    $('#register').show().prev().hide();
+// ----------------------------------------------------------切换
+$("#goto-register").on("click", function() {
+    // $("#login").hide();
+    $("#register").show();
+});
+$("#goto-login").on("click", function() {
+    // $("#login").show();
+    $("#register").hide();
 });
 
-$('#register a').click(function() {
-    $('#login').show().next().hide();
-});
 
-// .prev()  下一个兄弟;
 
+
+
+
+
+
+// -----------------------------------------------------------注册
+// 验证：记录下来；提高开发效果；
 var form = layui.form;
 form.verify({
-    // 规则名：[正则 不符合正则提醒信息]
+    // 规则名：[正则、不符合正则提醒信息]
     changdu: [/^\S{6,12}$/, "不满足长度要求"],
-    // 规则名：函数 必须要return 不符合正则提醒信息
+
+
+    // 规则名：函数，必须 有return  不符合正则提醒信息
     same: function(val) {
-        // 再次输入 val
-        // 第一次输入：直接获取;html结构上做一些简单类名补充 方便获取
+        // 第一输入：直接获取；HTML结构上做一些简单类名补充，方便获取值
+        // 再次输入：val
+
         if ($(".pwd").val() != val) {
             return "两次输入的密码不一致";
         }
     }
-
 });
 
-// --------------------------注册
 $("#register .layui-form").on("submit", function(e) {
-    // 1.阻止默认行为
+    // 1.阻止默认行为；
     e.preventDefault();
+
+
     // 2.收集数据
     var data = $(this).serialize();
-    // 3.接口 发出请求
+
+    // 3.接口，发出请求
+    // 发生数据
     $.ajax({
         type: 'POST',
-        url: 'http://ajax.frontend.itheima.net/api/reguser',
+        url: '/api/reguser',
         data: data,
         success: function(res) {
-            // 弹窗：msg（简单弹窗 会自动消失）
+            // 弹窗：msg 简单弹窗、会自动消失；
             layer.msg(res.message);
 
-            if (res.status === 0) {
-                // 注册成功，显示登录的盒子
-                $('#login').show().next().hide();
+            // 业务设计：
+            if (res.status == 0) {
+                $("#register").hide();
+                $("#login").show();
 
-                // 清空注册的表单(reset是dom方法，所以把jQuery对象转成DOM对象)
-                $('#register .layui-form')[0].reset();
+                // 需求：原生方式；重置！
+                //       注意慎重使用this;
+                $("#register .layui-form")[0].reset();
             }
         }
     });
+
+
+
 });
-// -----------------------------登陆
-$('#login form').on('submit', function(e) {
+
+
+
+
+// -----------------------------------------------------------登录
+$("#login .layui-form").on("submit", function(e) {
+    // 1.阻止默认行为；
     e.preventDefault();
 
-    // 收集账号、密码
+
+    // 2.收集数据
     var data = $(this).serialize();
-    // ajax提交
+
+    // 3.接口，发出请求
+    // 发生数据
     $.ajax({
         type: 'POST',
-        url: 'http://ajax.frontend.itheima.net/api/login',
+        url: '/api/login',
         data: data,
         success: function(res) {
-            // 无论登录成功，还是失败，都给提示
+            // 弹窗：msg 简单弹窗、会自动消失；
             layer.msg(res.message);
-            if (res.status === 0) {
+
+            if (res.status == 0) {
                 // 把token保存到本地存储
                 localStorage.setItem('token', res.token);
-                // 跳转到index.html
-                location.href = '../index.html';
+
+                // 
+                location.href = "../index.html";
             }
         }
     });
-})
+
+
+
+});
